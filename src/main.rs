ed58940540c3e1a321ba1fd707fd5d4c81079031
Muhammad <a1zenfps@pm.me>
2026-09-17 23:55:38 +0100

    added function call and return functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ struct CPU {
     poisition_in_memory: usize,
     registers: [u8; 16],
     memory: [u8; 0x1000],
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -11,6 +13,29 @@ impl CPU {
         let op_byte2 = self.memory[p + 1] as u16;
 
         op_byte1 << 8 | op_byte2
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("stack overflow");
+        }
+
+        stack[sp] = self.poisition_in_memory as u16;
+        self.stack_pointer += 1;
+        self.poisition_in_memory = addr as usize
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.poisition_in_memory = addr as usize;
     }
 
     fn run(&mut self) {
@@ -23,9 +48,13 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = (opcode & 0x000F) as u8;
 
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => return,
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
+                (0x2, _, _, _) => self.call(nnn),
+                (0, 0, 0xE, 0xE) => self.ret(),
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
@@ -51,21 +80,29 @@ fn main() {
         poisition_in_memory: 0,
         registers: [0; 16],
         memory: [0; 4096],
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80;
-    mem[1] = 0x14;
-    mem[2] = 0x80;
-    mem[3] = 0x24;
-    mem[4] = 0x80;
-    mem[5] = 0x34;
+
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
 
     cpu.run();
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + 10 = {}", cpu.registers[0]);
 }
